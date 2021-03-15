f23c85583c76d75ce109a111cc72e76519effc35
muncle <blueink38@yahoo.com>
2021-03-15 14:49:43 -0500

    updated from snapshot

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -107,6 +107,10 @@ app.get('/animals', (req, res) => {
 
 app.get('/zookeepers', (req, res) => {
   res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
